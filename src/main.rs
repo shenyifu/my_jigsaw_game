b5772e21b46a7e57a924f4f8c0c5428b5be4bdd9
shenyifu <406708433@qq.com>
2025-05-09 10:29:40 +0800

    draw someting on window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+    let shape = meshes.add(Circle::new(50.0));
+
+    let color = Color::hsl(360. as f32, 0.95, 0.7);
+
+    commands.spawn((
+        Mesh2d(shape),
+        MeshMaterial2d(materials.add(color)),
+        Transform::from_xyz(
+            0.0,
+            0.0,
+            0.0,
+        ),
+    ));
 }

a83b364bdf40847e8e1c71c3263efc0ca0f17a57
raymond <raymond@thenetcircle.com>
2025-05-27 10:38:02 +0800

    sprite follow cursor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 fn main() {
     App::new()
@@ -17,24 +18,28 @@ enum Direction {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
     commands.spawn((
-        Sprite::from_image(asset_server.load("resources/hat.png")),
+        Sprite::from_image(asset_server.load("resources/the-mortar.png")),
         Transform::from_xyz(0., 0., 0.),
         Direction::Right,
     ));
 }
 
-fn move_sprite(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
-    for (mut direction, mut transform) in sprite_position.iter_mut() {
-        match *direction {
-            Direction::Left => transform.translation.x -= 150. * time.delta_secs(),
-            Direction::Right => transform.translation.x += 150. * time.delta_secs(),
-        }
-        
-        if transform.translation.x > 200. { 
-            *direction = Direction::Left;
-        }
-        else if transform.translation.x < -200. {
-            *direction = Direction::Right;
+fn move_sprite(
+    mut sprite_position: Query<(&mut Direction, &mut Transform)>,
+    q_window: Query<&Window, With<PrimaryWindow>>,
+    q_camera: Query<(&Camera, &GlobalTransform)>,
+) {
+    let (camera, camera_transform) = q_camera.single().unwrap();
+    let window = q_window.single().unwrap();
+
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| Some(camera.viewport_to_world(camera_transform, cursor).unwrap()))
+        .map(|ray| ray.origin.truncate())
+    {
+        for (mut direction, mut transform) in sprite_position.iter_mut() {
+            transform.translation.x = world_position.x;
+            transform.translation.y = world_position.y;
         }
     }
 }

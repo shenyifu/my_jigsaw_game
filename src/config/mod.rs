de712fa551b24cc6a08c8765a1e73d0103c875eb
shenyifu <406708433@qq.com>
2025-06-06 16:41:57 +0800

    move config to config plugin

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,1 +1,60 @@
+use crate::config::total_pieces::TotalPieces;
+use crate::{
+    GameState, OnConfigScreen, PieceButton, despawn_screen, start_game, total_piece_button_click,
+};
+use bevy::prelude::*;
+use strum::IntoEnumIterator;
+
 pub mod total_pieces;
+
+pub fn config_plugin(app: &mut App) {
+    app.add_systems(OnEnter(GameState::Config), setup_config)
+        .add_systems(OnExit(GameState::Config), despawn_screen::<OnConfigScreen>);
+}
+
+fn setup_config(mut commands: Commands) {
+    let button_node = Node {
+        width: Val::Px(300.0),
+        height: Val::Px(65.0),
+        margin: UiRect::all(Val::Px(20.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..default()
+    };
+
+    let parent = commands
+        .spawn((
+            Node {
+                flex_direction: FlexDirection::Column,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            OnConfigScreen,
+        ))
+        .id();
+
+    for total_piece in TotalPieces::iter() {
+        let child = commands
+            .spawn((
+                Button,
+                button_node.clone(),
+                PieceButton { total_piece },
+                children![Text::new(total_piece.to_string())],
+                OnConfigScreen,
+            ))
+            .observe(total_piece_button_click)
+            .id();
+        commands.entity(parent).add_child(child);
+    }
+
+    let start_game = commands
+        .spawn((
+            Button,
+            button_node.clone(),
+            children![Text::new("start")],
+            OnConfigScreen,
+        ))
+        .observe(start_game)
+        .id();
+    commands.entity(parent).add_child(start_game);
+}
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use image::{DynamicImage, GenericImageView};
 use rand::{Rng, thread_rng};
 use std::cmp::PartialEq;
 use std::path::Path;
-use strum::IntoEnumIterator;
+
 
 // 3 * 2
 const PAINT_BOARD_HEIGHT: f32 = 640.;
@@ -36,10 +36,9 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        // for config
         .init_state::<GameState>()
-        .add_systems(OnEnter(GameState::Config), setup_config)
-        .add_systems(OnExit(GameState::Config), despawn_screen::<OnConfigScreen>)
+        .add_plugins((config::config_plugin))
+        // for config
         .insert_resource(TotalPieces::P6)
         // for play
         .add_systems(OnEnter(GameState::Play), setup_game)
@@ -91,54 +90,8 @@ struct PieceButton {
     total_piece: TotalPieces,
 }
 
-fn setup_config(mut commands: Commands) {
-    let button_node = Node {
-        width: Val::Px(300.0),
-        height: Val::Px(65.0),
-        margin: UiRect::all(Val::Px(20.0)),
-        justify_content: JustifyContent::Center,
-        align_items: AlignItems::Center,
-        ..default()
-    };
 
-    let parent = commands
-        .spawn((
-            Node {
-                flex_direction: FlexDirection::Column,
-                align_items: AlignItems::Center,
-                ..default()
-            },
-            OnConfigScreen,
-        ))
-        .id();
 
-    for total_piece in TotalPieces::iter() {
-        let child = commands
-            .spawn((
-                Button,
-                button_node.clone(),
-                PieceButton { total_piece },
-                children![Text::new(total_piece.to_string())],
-                OnConfigScreen,
-            ))
-            .observe(total_piece_button_click)
-            .id();
-        commands.entity(parent).add_child(child);
-    }
-
-    let start_game = commands
-        .spawn((
-            Button,
-            button_node.clone(),
-            children![Text::new("start")],
-            OnConfigScreen,
-        ))
-        .observe(start_game)
-        .id();
-    commands.entity(parent).add_child(start_game);
-}
-
-fn clean_config() {}
 
 fn start_game(click: Trigger<Pointer<Click>>, mut state: ResMut<NextState<GameState>>) {
     state.set(GameState::Play);
